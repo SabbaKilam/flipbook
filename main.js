bceafc86eecca0fd1b78836780cf4dc6347974df
sabbakilam1 <pitdrop@gmail.com>
2016-03-20 05:12:37 +0000

    small screen behavior better

--- main.js
+++ main.js
@@ -1,38 +1,45 @@
 /*global rekwire*/
-var msgArray = ["This","is","not","the","entire","story.","Abbas", "Malik", "Hello" ]
+var msgArray = ["Abbas<br/>Malik","This","is","not","the","entire","story.", "Hello" ]
 var _ = rekwire("module");
 var index = 0;
 _(window).on("load",function(){
     resizeRem();
 });
 _(window).on("resize",function(){
-    resizeRem();
+    _("#holder").styles
+        ("left","30rem")
+        ("width","25rem")           
+    ;    
+    if(window.innerWidth >= window.screen.width/2){
+        resizeRem();
+    }
+    else{
+        _("#holder").styles
+            ("left","2.5%")
+            ("width","95%")            
+        ;
+    }
+
 });
 
-_(".page").click(function(e){
+_(".page").on("mousedown", function(e){
     _(e.target).styles
-        ("transition","all 2s ease")         
-        ("transform","rotateY(-360deg)")
+        ("transition","all 2s ease")
+        ("transform","rotateY(360deg)")
         ("background","black")
-                ("color","black")        
-        //("opacity","0")            
+        ("color","black")
         ("zIndex","1")
-        //("visibility","hidden")            
-       //("transform","rotateY(0deg)")            
-        //("border","4px solid white")
     ;
     _(".page").array().forEach(function(m){
 
         var background = "gray";//"linear-gradient(hsla(35, 30%, 35%, 1.00),hsla(35, 30%, 35%, 0))";
         if(m !== e.target){
             _(m).styles
-                ("transition","z-index 1s ease")                
+                ("transition","z-index 1s ease")
                 ("transform","rotateY(0deg)")
                 ("background", background)
                 ("color","white")
-                //("border","none")
                 ("zIndex","2")
-                //("visibility","visible")
             ;
             //msgArray.push(msgArray.shift());
             if(index >= msgArray.length){
@@ -40,9 +47,8 @@ _(".page").click(function(e){
             };
             if(index === msgArray.length -1)_(m).css("background", "teal");
             _(m).html(msgArray[index]);
-            index++;            
+            index++;
         }
-        //else{_(m).styles("transitionDelay","all 5s ease")  }
     });
 });
 //--------
